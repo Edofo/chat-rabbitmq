99a3cc00bc4710630b66763f86e7a76ef912448a
Hivenet-Nolan <144666332+Hivenet-Nolan@users.noreply.github.com>
2024-06-04 14:11:33 +0200

    add web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,27 @@
+"use client";
+
 import styles from "./page.module.css";
+import { Client } from "@stomp/stompjs";
+import { useEffect } from "react";
+import { WebSocket } from "ws";
 
 export default function Page(): JSX.Element {
+  // Object.assign(global, { WebSocket });
+  useEffect(() => {
+    const client = new Client({
+      brokerURL: "ws://localhost:15674/ws",
+      connectHeaders: {
+        login: "user",
+        passcode: "password",
+      },
+    });
+
+    client.activate();
+
+    client.onConnect = () => {
+      console.log("Connected");
+    };
+  }, []);
+
   return <main className={styles.main}>aas</main>;
 }
