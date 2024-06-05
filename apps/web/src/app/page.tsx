f23f5735f1fa38ebca7ce3428a428d70465f79d9
bakarykone <bakarykone729@gmail.com>
2024-06-05 11:29:04 +0200

    front updated

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,6 +3,7 @@
 import { type Socket, io } from "socket.io-client";
 import styles from "./page.module.css";
 import { useEffect, useState } from "react";
+import { Component } from "@/components/component/component";
 
 export default function Page(): JSX.Element {
   const [socket, setSocket] = useState<Socket | null>(null);
@@ -27,6 +28,7 @@ export default function Page(): JSX.Element {
   return (
     <div>
       <h1>Socket.io</h1>
+      <Component />
       <button onClick={sendMessage}>Send message</button>
     </div>
   );
